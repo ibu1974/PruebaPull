6a3b5d42998dc857a31b8ca64e678d40050b41a2
ibu <israel.bravo@gnp.com.mx>
2020-05-11 16:59:36 -0500

    Es el primer cambio para sobre escritura

--- PruebaPull/src/gnp/com/ae/ClaseAE.java
+++ PruebaPull/src/gnp/com/ae/ClaseAE.java
@@ -1,7 +1,7 @@
 package gnp.com.ae;
 
 public class ClaseAE {
-//** Es el primer cambio **//
+//** Es el primer cambio para sobre escritura**//
 //*Es el segundo ccambio **//
 //*Es el tercer cambio **//	
 }
